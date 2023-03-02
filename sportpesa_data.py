a74999d875905c8090cf824a148c7b65b3af9cd3
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-03-02 08:21:22 +0300

    Compartmentalize

--- sportpesa_data.py
+++ sportpesa_data.py
@@ -5,8 +5,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Home Page
+HOME_PAGE_URL = "https://www.ke.sportpesa.com/sports/football?sportId=1&section=highlights"
 # Daily PreMatch events link.
-target_url = "https://www.ke.sportpesa.com/sports/football?sportId=1&section=today"
+DAILY_EVENTS_URL = "https://www.ke.sportpesa.com/sports/football?sportId=1&section=today"
+# Next Page Static url
+NEXT_PAGE_STATIC_URL = "https://www.ke.sportpesa.com/sports/football?sportId=1&section=today&paginationOffset="
 
 # Create a new ChromeDriver service object
 service = webdriver.chrome.service.Service(
@@ -15,59 +19,72 @@ service = webdriver.chrome.service.Service(
 # Start a new Chrome browser instance using the service object
 driver = webdriver.Chrome(service=service)
 
-driver.get(target_url)
-driver.maximize_window()
 
-try:
-    # Scroll to the bottom to ensure all events load
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+"""
+This function (get_general_data) returns an array of events with only general data.
+General data here refers to:
+    - Involved teams in an event. 
+    - Starting time of the event. 
+    - ID of the said event.
+"""
 
-    # Get a list with the number of available pages of events.
-    pagination = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located(
-            (By.CLASS_NAME, "event-list-pagination"))
-    )
 
-    pagination = driver.find_element(By.CLASS_NAME, "event-list-pagination")
+def get_general_data():
 
-    next_buttons = pagination.find_elements(By.CLASS_NAME, "ng-binding")
+    driver.get(DAILY_EVENTS_URL)
+    driver.maximize_window()
 
-    available_pages = [item.text for item in next_buttons]
+    try:
+        # Scroll to the bottom to ensure all events load
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
-    # Initiate an empty array to append dictionaries containing event info
-    result = []
+        # Get a list with the number of available pages of events.
+        pagination = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "event-list-pagination"))
+        )
 
-    event_totals = 15
-    # Get content from as many pages as ones available
-    for idx in range(len(available_pages[1:])):
-        # Get all the event rows
-        event_rows = driver.find_elements(
-            By.CLASS_NAME, "event-markets-count-4")
+        pagination = driver.find_element(
+            By.CLASS_NAME, "event-list-pagination")
 
-        # Get the start_time, ID and names of teams in each the match
-        for event in event_rows:
-            event_info = event.find_element(By.CLASS_NAME, "event-info")
-            entry_data = event_info.text.split("\n")
+        next_buttons = pagination.find_elements(By.CLASS_NAME, "ng-binding")
 
-            event_names = event.find_element(By.CLASS_NAME, "event-names")
-            entry_names = event_names.text.split("\n")
+        available_pages = [item.text for item in next_buttons]
 
-            entry = {}
-            entry["teams"] = f"{entry_names[0]} vs {entry_names[1]}"
-            entry["start_time"] = f"{entry_data[0]}"
-            entry["event_id"] = entry_data[2]
+        # Initiate an empty array to append dictionaries containing event info
+        result = []
 
-            result.append(entry)
+        event_totals = 15
+        # Get content from as many pages as ones available
+        for idx in range(len(available_pages[1:])):
+            # Get all the event rows
+            event_rows = driver.find_elements(
+                By.CLASS_NAME, "event-markets-count-4")
 
-        # Get the URL for the next page
-        next_page_url = f"https://www.ke.sportpesa.com/sports/football?sportId=1&section=today&paginationOffset={event_totals}"
+            # Get the start_time, ID and names of teams in each the match
+            for event in event_rows:
+                event_info = event.find_element(By.CLASS_NAME, "event-info")
+                entry_data = event_info.text.split("\n")
 
-        # Navigate to the next page
-        driver.get(next_page_url)
+                event_names = event.find_element(By.CLASS_NAME, "event-names")
+                entry_names = event_names.text.split("\n")
 
-        event_totals += 15          # Total number of events sportpesa loads per page
+                entry = {}
+                entry["teams"] = f"{entry_names[0]} vs {entry_names[1]}"
+                entry["start_time"] = f"{entry_data[0]}"
+                entry["event_id"] = entry_data[2]
 
-finally:
-    for entry in result:
-        print(entry)
-    driver.quit()
+                result.append(entry)
+
+            # Get the URL for the next page
+            next_page_url = NEXT_PAGE_STATIC_URL + str(event_totals)
+
+            # Navigate to the next page
+            driver.get(next_page_url)
+
+            event_totals += 15          # Total number of events sportpesa loads per page
+
+    finally:
+        driver.quit()
+
+    return result
