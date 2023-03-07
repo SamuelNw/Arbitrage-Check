d34773264956b2bf82d4d79591fc046207671c77
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-03-07 12:47:59 +0300

    Get all required data

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
 # Use data from sportpesa to search through betika.com and combine all that to a csv file.
+import sportpesa_data
+import betika_data
+
+
+initial_data = sportpesa_data.get_sportpesa_data()
+updated_array = betika_data.add_betika_data(initial_data)
+
+
+for idx, item in enumerate(updated_array):
+    print(f"{idx} : {item}")
